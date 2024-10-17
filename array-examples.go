2e4796e941475fbf8171d478487ae7c7a7b29c93
Ediyyesmi <143936492+Ediyyesmi@users.noreply.github.com>
2024-10-17 16:47:09 +0300

    Update array-examples.go

--- array-examples.go
+++ array-examples.go
@@ -1,1 +1,24 @@
+package tekrar
 
+import "fmt"
+
+func Demo1(){
+
+	var m, n int= 0,0
+	var tek[5] int
+	var cift[5] int 
+	for i:=0;i<10;i++ {
+		if i%2!=0 {
+			tek[n]=i
+			n++
+		}else{
+			cift[m]=i
+			m++
+		}
+	}
+	
+	fmt.Println("tek sayilar: ",tek)
+	fmt.Println("cift sayilar: ",cift)
+
+
+}
